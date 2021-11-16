5c6984bc880f971f246fd6d49f1efe51daf0761a
homeBeaver <revirthuum@gmail.com>
2021-11-17 00:54:00 +0900

    외부 데이터 로드

--- study2/init.js
+++ study2/init.js
@@ -5,14 +5,22 @@ class Init extends Phaser.Scene
         super();
     }
 
+    // 외부 데이터 로드
     preload ()
     {
       // phaser labs url
       this.load.setBaseURL("https://labs.phaser.io/");
+      this.load.image('sky', 'assets/sky.png');
+      this.load.image('ground', 'assets/platform.png');
+      this.load.image('star', 'assets/star.png');
+      this.load.spritesheet('dude', 'assets/dude.png', {
+        frameWidth: 32,
+        frameHeight: 48,
+      });
     }
 
     create ()
     {
-      
+
     }
 }
