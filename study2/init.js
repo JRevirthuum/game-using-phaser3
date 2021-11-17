ad9363f2629a569da899f84b36dfc0b3db2b41c0
OneplatBeaver <beaver@oneplat.co>
2021-11-17 14:26:32 +0900

    움직임 추가
    점프를 2 단계로 구분

--- study2/init.js
+++ study2/init.js
@@ -10,6 +10,7 @@ class Init extends Phaser.Scene
     {
       // phaser labs url
       this.load.setBaseURL("https://labs.phaser.io/src/games/firstgame/");
+
       this.load.image('sky', 'assets/sky.png');
       this.load.image('ground', 'assets/platform.png');
       this.load.image('star', 'assets/star.png');
@@ -31,6 +32,27 @@ class Init extends Phaser.Scene
       platforms.create(50, 250, 'ground');
       platforms.create(750, 220, 'ground');
 
+      this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+        frameRate: 10,
+        repeat: -1
+      });
+
+      this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+        frameRate: 10,
+        repeat: -1
+      });
+
+      this.anims.create({
+        key: 'normal',
+        frames:[{ 
+          key: 'dude', frame: 4 
+        }],
+        frameRate: 20
+      })
 
       // 유저 생성
       player = this.physics.add.sprite(100, 450, 'dude');
@@ -44,5 +66,36 @@ class Init extends Phaser.Scene
       this.physics.add.collider(player, platforms);
 
       //this.add.image(400,300, 'star');
+      this.cursors = this.input.keyboard.createCursorKeys();
+      this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+      
+      // 2단점프 체크용
+      this.jump_count = 0;
+    }
+
+    update() 
+    {
+      
+      if(this.cursors.left.isDown) {
+        player.setVelocityX(-200);
+        player.anims.play('left', true);
+      } else if(this.cursors.right.isDown) {
+        player.setVelocityX(200);
+        player.anims.play('right', true);
+      } else {
+        player.setVelocityX(0);
+        player.anims.play('normal');
+      }
+
+      if(Phaser.Input.Keyboard.JustDown(this.spacebar)) {
+        if(!player.body.touching.none){
+          this.jump_count = 0;
+        }        
+        if(this.jump_count < 2) {
+          player.setVelocityY(-1000);
+          this.jump_count++;
+        }
+        console.log(this.jump_count);
+      }
     }
 }
